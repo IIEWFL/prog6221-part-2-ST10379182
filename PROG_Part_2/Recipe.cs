9715a01b85de16af54b3d03eed8238a56107c7ed
Liam <131532595+ST10379182@users.noreply.github.com>
2024-05-29 22:36:24 +0200

    Commit 4

--- PROG_Part_2/Recipe.cs
+++ PROG_Part_2/Recipe.cs
@@ -9,7 +9,9 @@ namespace RecipeApp
 
     public class Recipe
     {
-  
+        public string Name { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+        public List<string> Steps { get; set; }
 /*https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/
 https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/exceptions/
 https://learn.microsoft.com/en-us/dotnet/api/system.console?view=net-8.0

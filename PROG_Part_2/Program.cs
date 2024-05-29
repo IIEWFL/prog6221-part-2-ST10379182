22c650e57552a141e0595ddbcf87d4d0360968a4
Liam <131532595+ST10379182@users.noreply.github.com>
2024-05-29 22:34:01 +0200

    Commit 2

--- PROG_Part_2/Program.cs
+++ PROG_Part_2/Program.cs
@@ -5,4 +5,12 @@ namespace RecipeApp
     //Main Program Class
     class Program
     {
-      
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("\nOptions:");
+                Console.WriteLine("1. Add a new recipe");
+                Console.WriteLine("2. List all recipes");
+                Console.WriteLine("3. Select a recipe to display");
+                Console.WriteLine("4. Exit");

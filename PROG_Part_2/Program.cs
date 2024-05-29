e4f16c69ec0896d251468e1cde9283b4d98e399d
Liam <131532595+ST10379182@users.noreply.github.com>
2024-05-29 22:39:23 +0200

    Commit 6

--- PROG_Part_2/Program.cs
+++ PROG_Part_2/Program.cs
@@ -14,3 +14,7 @@ namespace RecipeApp
                 Console.WriteLine("2. List all recipes");
                 Console.WriteLine("3. Select a recipe to display");
                 Console.WriteLine("4. Exit");
+                Console.Write("Choose an option: ");
+                int option = int.Parse(Console.ReadLine());
+
+                switch (option)

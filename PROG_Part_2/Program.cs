bd4d26c3050d4fd83e83c26b2ef6698bacbc3dcd
Liam <131532595+ST10379182@users.noreply.github.com>
2024-05-29 22:54:14 +0200

    Commit 14

--- PROG_Part_2/Program.cs
+++ PROG_Part_2/Program.cs
@@ -14,12 +14,28 @@ namespace RecipeApp
                 Console.WriteLine("2. List all recipes");
                 Console.WriteLine("3. Select a recipe to display");
                 Console.WriteLine("4. Exit");
+
                 Console.Write("Choose an option: ");
                 int option = int.Parse(Console.ReadLine());
 
                 switch (option)
-                 switch (option)
                 {
                     case 1:
                         RecipeManager.AddRecipe();
                         break;
+                    case 2:
+                        RecipeManager.ListRecipes();
+                        break;
+                    case 3:
+                        RecipeManager.SelectAndDisplayRecipe();
+                        break;
+                    case 4:
+                        return;
+                }
+            }
+        }
+    }
+}
+/*https://learn.microsoft.com/en-us/dotnet/api/system.console?view=net-8.0 
+
+ */

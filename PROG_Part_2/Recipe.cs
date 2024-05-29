07c657ff7b4aed719297f658679f108e66d1c6bb
Liam <131532595+ST10379182@users.noreply.github.com>
2024-05-29 22:41:18 +0200

    Commit 8

--- PROG_Part_2/Recipe.cs
+++ PROG_Part_2/Recipe.cs
@@ -12,6 +12,14 @@ namespace RecipeApp
         public string Name { get; set; }
         public List<Ingredient> Ingredients { get; set; }
         public List<string> Steps { get; set; }
+        public event CalorieNotification OnHighCalories;
+
+        public Recipe(string name)
+        {
+            Name = name;
+            Ingredients = new List<Ingredient>();
+            Steps = new List<string>();
+
 /*https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/
 https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/exceptions/
 https://learn.microsoft.com/en-us/dotnet/api/system.console?view=net-8.0

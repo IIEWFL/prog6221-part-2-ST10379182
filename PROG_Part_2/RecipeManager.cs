b0d51f2a0032e318ad35369fad2399fab108a33a
Liam <131532595+ST10379182@users.noreply.github.com>
2024-05-29 22:37:51 +0200

    Commit 5

--- PROG_Part_2/RecipeManager.cs
+++ PROG_Part_2/RecipeManager.cs
@@ -4,4 +4,11 @@ using System.Collections.Generic;
 
 namespace RecipeApp
 {
- 
+
+    private static List<Recipe> recipes = new List<Recipe>();
+
+    //Add a new recipe
+    public static void AddRecipe()
+    {
+        Console.Write("Enter the name of the recipe: ");
+        string name = Console.ReadLine();

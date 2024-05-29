5954c71adad24cae0e7da446680ed3e12235bf0b
Liam <131532595+ST10379182@users.noreply.github.com>
2024-05-29 22:31:55 +0200

    Start of Program

--- PROG_Part_2/Ingredient.cs
+++ PROG_Part_2/Ingredient.cs
@@ -5,33 +5,4 @@ namespace RecipeApp
     //Ingredient Class
     public class Ingredient
     {
-        public string Name { get; set; }
-        public double Quantity { get; set; }
-        public string Unit { get; set; }
-        public int Calories { get; set; }
-        public string FoodGroup { get; set; }
-
-        public Ingredient(string name, double quantity, string unit, int calories, string foodGroup)
-        {
-            Name = name;
-            Quantity = quantity;
-            Unit = unit;
-            Calories = calories;
-            FoodGroup = foodGroup;
-        }
-        //Method to scale ingredients
-        public void Scale(double factor)
-        {
-            Quantity *= factor;
-        }
-
-        public override string ToString()
-        {
-            return $"{Quantity} {Unit} of {Name} ({Calories} calories, {FoodGroup})";
-        }
-    }
-}
-/*https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/arrays
-https://learn.microsoft.com/en-us/dotnet/csharp/
-https://learn.microsoft.com/en-us/dotnet/api/system.console?view=net-8.0
-*/
+ 
--- PROG_Part_2/Program.cs
+++ PROG_Part_2/Program.cs
@@ -5,37 +5,4 @@ namespace RecipeApp
     //Main Program Class
     class Program
     {
-        static void Main(string[] args)
-        {
-            while (true)
-            {
-                Console.WriteLine("\nOptions:");
-                Console.WriteLine("1. Add a new recipe");
-                Console.WriteLine("2. List all recipes");
-                Console.WriteLine("3. Select a recipe to display");
-                Console.WriteLine("4. Exit");
-
-                Console.Write("Choose an option: ");
-                int option = int.Parse(Console.ReadLine());
-
-                switch (option)
-                {
-                    case 1:
-                        RecipeManager.AddRecipe();
-                        break;
-                    case 2:
-                        RecipeManager.ListRecipes();
-                        break;
-                    case 3:
-                        RecipeManager.SelectAndDisplayRecipe();
-                        break;
-                    case 4:
-                        return;
-                }
-            }
-        }
-    }
-}
-/*https://learn.microsoft.com/en-us/dotnet/api/system.console?view=net-8.0 
-
- */
+      

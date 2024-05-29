6645d0b3599659536cf347ccfe77b32e2dde44f3
Liam <131532595+ST10379182@users.noreply.github.com>
2024-05-29 22:57:08 +0200

    Final Commit (Commit 15)

--- PROG_Part_2/Ingredient.cs
+++ PROG_Part_2/Ingredient.cs
@@ -10,8 +10,29 @@ namespace RecipeApp
         public string Unit { get; set; }
         public int Calories { get; set; }
         public string FoodGroup { get; set; }
+
         public Ingredient(string name, double quantity, string unit, int calories, string foodGroup)
         {
             Name = name;
             Quantity = quantity;
+            Unit = unit;
+            Calories = calories;
+            FoodGroup = foodGroup;
+        }
+        //Method to scale ingredients
+        public void Scale(double factor)
+        {
+            Quantity *= factor;
+        }
 
+        public override string ToString()
+        {
+            return $"{Quantity} {Unit} of {Name} ({Calories} calories, {FoodGroup})";
+        }
+    }
+}
+
+/*https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/arrays
+https://learn.microsoft.com/en-us/dotnet/csharp/
+https://learn.microsoft.com/en-us/dotnet/api/system.console?view=net-8.0
+*/
